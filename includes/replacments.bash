d65b1260a59e15a255c2834bbe11b1d5e2474539
ParsaKhaledi <pa.aerospace@gmail.com>
2025-01-18 16:13:31 +0330

    add apt world and replacment script

--- includes/replacments.bash
+++ includes/replacments.bash
@@ -2,6 +2,6 @@
 
 rm -f /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt
 cp -r /volume/includes/CMakeLists.txt /PX4-Autopilot/src/modules/simulation/gz_bridge/
-cp -r /volume/husarion_gz_worlds/models/* /PX4-Autopilot/Tools/simulation/gz/models/
-cp -r /volume/husarion_gz_worlds/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
+cp -r /volume/includes/models/* /PX4-Autopilot/Tools/simulation/gz/models/
+cp -r /volume/includes/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
 echo "Done."
