b18f23f2be75ec6a3f9a50d2f4440dd40a52abc0
ParsaKhaledi <pa.aerospace@gmail.com>
2025-02-11 17:34:06 +0330

    gz_start_rtbamap_rgbd file update

---
+++ startFiles/gz_start_rtabmap_rgbd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /ws_px4_ros2/install/setup.bash
+ros2 launch rtabmap_launch rtabmap.launch.py   \
+     args:='-d  --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10 --Kp/DetectorStrategy 10 --Grid/MapFrameProjection true \
+     --NormalsSegmentation false --Grid/MaxGroundHeight 0.5  --Grid/MaxObstacleHeight 2.2 --RGBD/StartAtOrigin true       \
+     '    \
+     rgb_topic:=/camera/rgb/image_raw   depth_topic:=/camera/depth/image_raw    camera_info_topic:=/camera/rgb/camera_info \
+     imu_topic:=/imu approx_sync:=true    \
+     use_sim_time:=true  qos:=2    rtabmapviz:=true     rviz:=false   subscribe_rgbd:=false
