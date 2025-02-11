cd9995aa2f6c321e1464f46da0dca51fce3ec86f
ParsaKhaledi <pa.aerospace@gmail.com>
2025-02-11 18:45:40 +0330

    fix some issues

--- includes/replacments_gz.bash
+++ includes/replacments_gz.bash
@@ -4,19 +4,21 @@ while true; do
     read input
 
     if [ "$input" -eq 1 ]; then
-        rm -f /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt
+        rm -rf /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
+              /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
         cp -r /volume/includes/gz/CMakeLists.txt /PX4-Autopilot/src/modules/simulation/gz_bridge/
         cp -r /volume/includes/gz/models/* /PX4-Autopilot/Tools/simulation/gz/models/
         cp -r /volume/includes/gz/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
-        mv    /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-stereo /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
+        mv -v /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-stereo /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
         echo "Replacments with stereo camera is done"
         break
     elif [ "$input" -eq 2 ]; then
-        rm -f /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt
+        rm -rf /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
+              /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
         cp -r /volume/includes/gz/CMakeLists.txt /PX4-Autopilot/src/modules/simulation/gz_bridge/
         cp -r /volume/includes/gz/models/* /PX4-Autopilot/Tools/simulation/gz/models/
         cp -r /volume/includes/gz/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
-        mv    /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-rgbd /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
+        mv -v /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-rgbd /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
         echo "Replacments with rgbd camera is done"
         break
     else
