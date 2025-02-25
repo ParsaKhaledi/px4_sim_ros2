10d446271583e73fb9ab93d56f4bf559583732f6
ParsaKhaledi <pa.aerospace@gmail.com>
2025-02-25 18:47:46 +0330

    change imu topic

--- startFiles/gz_start_rtabmap_stereo.sh
+++ startFiles/gz_start_rtabmap_stereo.sh
@@ -9,6 +9,6 @@ ros2 launch rtabmap_launch rtabmap.launch.py \
      stereo:=true  \
     left_image_topic:=/camera/stereo/left/image_raw    left_camera_info_topic:=/camera/stereo/left/camera_info    \
     right_image_topic:=/camera/stereo/right/image_raw  right_camera_info_topic:=/camera/stereo/right/camera_info   \
-    imu_topic:=/rtabmap/imu  frame_id:=oak-d-base-frame  \
+    imu_topic:=/imu  frame_id:=oak-d-base-frame  \
     approx_sync:=true  wait_imu_to_init:=true  approx_sync_max_interval:=0.001  \
-    qos:=2  rtabmapviz:=false  rviz:=false
+    qos:=2  rtabmapviz:=true  rviz:=false
