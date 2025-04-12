28ecb955356d54ff9498f7ec6d8bf0675753f3a1
ParsaKhaledi <pa.aerospace@gmail.com>
2025-04-12 12:33:37 +0330

    combine rtabmap start files

---
+++ includes/gz/startFiles/gz_start_rtabmap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+# CamerType=$1
+
+if [ "$CamerType" = Stereo ] || [ "$CamerType" = stereo ]; then
+     echo "Run Rtabmap with $CamerType camera"
+     ros2 launch rtabmap_launch rtabmap.launch.py \
+          args:="-d --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10  --Kp/DetectorStrategy 10  \
+          --Grid/MapFrameProjection true  --NormalsSegmentation false --Grid/MaxGroundHeight 1.0 \
+          --Grid/MaxObstacleHeight 2.0 --RGBD/StartAtOrigin true" \
+          stereo:=true  \
+          left_image_topic:=/camera/stereo/left/image_raw    left_camera_info_topic:=/camera/stereo/left/camera_info    \
+          right_image_topic:=/camera/stereo/right/image_raw  right_camera_info_topic:=/camera/stereo/right/camera_info   \
+          imu_topic:=/imu  frame_id:=oak-d-base-frame  \
+          approx_sync:=true  wait_imu_to_init:=true  approx_sync_max_interval:=0.001  \
+          qos:=2  rtabmapviz:=true  rviz:=false
+elif [ "$CamerType" = rgbd ] || [ "$CamerType" = RGBD ] ; then
+     echo "Run Rtabmap with $CamerType camera"
+     ros2 launch rtabmap_launch rtabmap.launch.py   \
+          args:='-d  --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10 --Kp/DetectorStrategy 10 --Grid/MapFrameProjection true  \
+          --NormalsSegmentation false --Grid/MaxGroundHeight 0.5  --Grid/MaxObstacleHeight 2.2 --RGBD/StartAtOrigin true   '    \
+          rgb_topic:=/camera/rgb/image_raw   depth_topic:=/camera/depth/image_raw    camera_info_topic:=/camera/rgb/camera_info \
+          imu_topic:=/imu approx_sync:=true    \
+          use_sim_time:=true  qos:=2    rtabmapviz:=true     rviz:=false   subscribe_rgbd:=false
+else
+    echo "Invalid CamerType"
+    exit 1
+fi
