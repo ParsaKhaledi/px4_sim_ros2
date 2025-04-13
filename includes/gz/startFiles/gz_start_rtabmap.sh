e4b315c7c976fa7a1e9cb119b3f9c11dab889256
ParsaKhaledi <pa.aerospace@gmail.com>
2025-04-13 15:29:26 +0330

    remove frame _id

--- includes/gz/startFiles/gz_start_rtabmap.sh
+++ includes/gz/startFiles/gz_start_rtabmap.sh
@@ -12,9 +12,9 @@ if [ "$CamerType" = Stereo ] || [ "$CamerType" = stereo ]; then
           stereo:=true  \
           left_image_topic:=/camera/stereo/left/image_raw    left_camera_info_topic:=/camera/stereo/left/camera_info    \
           right_image_topic:=/camera/stereo/right/image_raw  right_camera_info_topic:=/camera/stereo/right/camera_info   \
-          imu_topic:=/imu  frame_id:=oak-d-base-frame  \
+          imu_topic:=/imu   \
           approx_sync:=true  wait_imu_to_init:=true  approx_sync_max_interval:=0.001  \
-          qos:=2  rtabmapviz:=true  rviz:=false
+          qos:=2  rtabmapviz:=true  rviz:=false # frame_id:=oak-d-base-frame 
 elif [ "$CamerType" = rgbd ] || [ "$CamerType" = RGBD ] ; then
      echo "Run Rtabmap with $CamerType camera"
      ros2 launch rtabmap_launch rtabmap.launch.py   \
