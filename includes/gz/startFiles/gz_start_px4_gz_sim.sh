7a00ccd308f0589fdc4551b52fdfc8edded3b71c
ParsaKhaledi <pa.aerospace@gmail.com>
2025-04-13 18:33:51 +0330

    add world input

--- includes/gz/startFiles/gz_start_px4_gz_sim.sh
+++ includes/gz/startFiles/gz_start_px4_gz_sim.sh
@@ -2,7 +2,7 @@
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 cd  /px4/PX4-Autopilot && \
-    PX4_GZ_WORLD=apt_world \
+    PX4_GZ_WORLD=$2 \
     PX4_GZ_MODEL_POSE="-3,-1.6,0,0,0,3.14" \
     make px4_sitl gz_x500_depth
 
