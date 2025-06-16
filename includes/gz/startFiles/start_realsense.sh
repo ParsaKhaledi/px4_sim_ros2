1188a8dc721548c45cf18ac7981fa795de3305f0
ParsaKhaledi <pa.aerospace@gmail.com>
2025-06-17 00:46:19 +0330

    Update addresses in startFiles

--- includes/gz/startFiles/start_realsense.sh
+++ includes/gz/startFiles/start_realsense.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+USER_NAME=px4
+HOME=/home/${USER_NAME}
+WORKDIR=/home/${USER_NAME}/ws_px4
+
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /ws_px4_ros2/install/setup.bash
+source ${WORKDIR}/install/setup.bash
+
 ros2 launch realsense2_camera rs_launch.py 
 
