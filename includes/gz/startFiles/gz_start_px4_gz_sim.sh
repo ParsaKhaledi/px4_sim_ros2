524845ab8d6fc3546af752915d4178f5c96c3604
ParsaKhaledi <pa.aerospace@gmail.com>
2025-06-22 20:56:24 +0330

    remove mavlink udp ip

--- includes/gz/startFiles/gz_start_px4_gz_sim.sh
+++ includes/gz/startFiles/gz_start_px4_gz_sim.sh
@@ -10,6 +10,5 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 cd ${HOME}/PX4-Autopilot && \
     PX4_GZ_WORLD=$1 \
     PX4_GZ_MODEL_POSE="-3,-1.6,0,0,0,3.14" \
-    MAVLINK_UDP_URI=udp://qgc:14550 \
-        make px4_sitl gz_x500_depth 
+    make px4_sitl gz_x500_depth 
 # PX4_GZ_WORLD=${World} 
