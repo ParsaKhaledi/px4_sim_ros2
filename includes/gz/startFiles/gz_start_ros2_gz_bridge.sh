11acb538b9f3783d6734b7eabc787f3decc99009
ParsaKhaledi <pa.aerospace@gmail.com>
2025-06-22 03:30:28 +0330

    fix addresses

--- includes/gz/startFiles/gz_start_ros2_gz_bridge.sh
+++ includes/gz/startFiles/gz_start_ros2_gz_bridge.sh
@@ -6,4 +6,4 @@ WORKDIR=/home/${USER_NAME}/ws_px4
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 ros2 run ros_gz_bridge parameter_bridge --ros-args -p \
-     config_file:=${HOME}/includes/gz/config_gz_bridge.yaml
+     config_file:=${HOME}/volume/includes/gz/config_gz_bridge.yaml
--- includes/gz/startFiles/gz_start_state_publisher.sh
+++ includes/gz/startFiles/gz_start_state_publisher.sh
@@ -6,7 +6,7 @@ WORKDIR=/home/${USER_NAME}/ws_px4
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-cd ${HOME}/volume/includes/gz/
+cd ${HOME}/volume/includes/gz/x500_tf_publisher/
 ros2 launch robot_state_publisher.launch.py
 
 # Update addresses
