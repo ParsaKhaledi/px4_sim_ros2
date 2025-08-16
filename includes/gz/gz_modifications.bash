7deff25697a4df763774d36e455eed8cb3dd2c73
ParsaKhaledi <pa.aerospace@gmail.com>
2025-08-16 15:44:29 +0330

    add  COM_RC_LOSS_T and NAV_RCL_ACT to be changed in running sim

--- includes/gz/gz_modifications.bash
+++ includes/gz/gz_modifications.bash
@@ -3,6 +3,10 @@
 echo "Start Modifications for running Simulation"
 input=$1
 WORKDIR=/home/px4
+# Set Custome params:
+echo "param set-default COM_RC_LOSS_T 35.0" >> $WORKDIR/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/px4-rc.params
+echo "param set-default NAV_RCL_ACT 1" >> $WORKDIR/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/px4-rc.params
+# Camera Modifications:
 echo "Selected Camera Type: $input"
 
 if [ "$input" = Stereo ] || [ "$input" = stereo ]; then
