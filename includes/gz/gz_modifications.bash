5154839a77db63ac5045cf49e91326f0f918f337
Parsa Khaledi <pa.aerospace@gmail.com>
2026-06-29 09:05:40 +0330

    Align GZ simulation assets with PX4 1.17 runtime overlay.
    Stop replacing upstream gz_bridge CMakeLists, harmonize ros_gz types to gz.msgs, fix world selection via apt_world.sdf, and update the SITL startup script to use the correct make targets.

--- includes/gz/gz_modifications.bash
+++ includes/gz/gz_modifications.bash
@@ -3,58 +3,25 @@
 echo "Start Modifications for running Simulation"
 input=$1
 WORKDIR=/home/px4
-# Set Custome params:
+# Set custom params:
 echo "param set-default COM_RC_LOSS_T 35.0" >> $WORKDIR/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/px4-rc.params
 echo "param set-default NAV_RCL_ACT 1" >> $WORKDIR/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/px4-rc.params
 # Camera Modifications:
 echo "Selected Camera Type: $input"
 
 if [ "$input" = Stereo ] || [ "$input" = stereo ]; then
-    rm -rf $WORKDIR/PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
-           $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-    cp -rv $WORKDIR/volume/includes/gz/CMakeLists.txt $WORKDIR/PX4-Autopilot/src/modules/simulation/gz_bridge/
+    rm -rf $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
     cp -rv $WORKDIR/volume/includes/gz/models/* $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/
     cp -rv $WORKDIR/volume/includes/gz/worlds/* $WORKDIR/PX4-Autopilot/Tools/simulation/gz/worlds/
     mv -v  $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-stereo $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-    echo "Replacments with $input camera is done"
+    echo "Replacements with $input camera is done"
 elif [ "$input" = rgbd ] || [ "$input" = RGBD ] ; then
-    rm -rf $WORKDIR/PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
-           $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-    cp -rv $WORKDIR/volume/includes/gz/CMakeLists.txt $WORKDIR/PX4-Autopilot/src/modules/simulation/gz_bridge/
+    rm -rf $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
     cp -rv $WORKDIR/volume/includes/gz/models/* $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/
     cp -rv $WORKDIR/volume/includes/gz/worlds/* $WORKDIR/PX4-Autopilot/Tools/simulation/gz/worlds/
     mv -v  $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-rgbd $WORKDIR/PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-    echo "Replacments with $input camera is done"
+    echo "Replacements with $input camera is done"
 else
     echo "Invalid input, please try again."
     exit 1
 fi
-
-
-
-# while true; do
-#     echo "For Stereo: 1  FOR RGBD: 2"
-#     read input
-
-#     if [ "$input" -eq 1 ]; then
-#         rm -rf /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
-#               /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-#         cp -rv /volume/includes/gz/CMakeLists.txt /PX4-Autopilot/src/modules/simulation/gz_bridge/
-#         cp -rv /volume/includes/gz/models/* /PX4-Autopilot/Tools/simulation/gz/models/
-#         cp -rv /volume/includes/gz/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
-#         mv -v /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-stereo /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-#         echo "Replacments with stereo camera is done"
-#         break
-#     elif [ "$input" -eq 2 ]; then
-#         rm -rf /PX4-Autopilot/src/modules/simulation/gz_bridge/CMakeLists.txt \
-#               /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-#         cp -rv /volume/includes/gz/CMakeLists.txt /PX4-Autopilot/src/modules/simulation/gz_bridge/
-#         cp -rv /volume/includes/gz/models/* /PX4-Autopilot/Tools/simulation/gz/models/
-#         cp -rv /volume/includes/gz/worlds/* /PX4-Autopilot/Tools/simulation/gz/worlds/
-#         mv -v /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite-rgbd /PX4-Autopilot/Tools/simulation/gz/models/OakD-Lite
-#         echo "Replacments with rgbd camera is done"
-#         break
-#     else
-#         echo "Invalid input, please try again."
-#     fi
-# done
--- includes/gz/startFiles/gz_start_px4_gz_sim.sh
+++ includes/gz/startFiles/gz_start_px4_gz_sim.sh
@@ -6,9 +6,16 @@ WORKDIR=/home/${USER_NAME}/ws_px4
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 
+WORLD="${1:-default}"
+MODEL="x500_depth"
 
-cd ${HOME}/PX4-Autopilot && \
-    PX4_GZ_WORLD=$1 \
-    PX4_GZ_MODEL_POSE="-3,-1.6,0,0,0,3.14" \
-    make px4_sitl gz_x500_depth 
-# PX4_GZ_WORLD=${World} 
+cd "${HOME}/PX4-Autopilot" || exit 1
+
+export PX4_GZ_MODEL_POSE="-3,-1.6,0,0,0,3.14"
+
+if [ "${WORLD}" = "default" ] || [ -z "${WORLD}" ]; then
+    make px4_sitl "gz_${MODEL}"
+else
+    export PX4_GZ_WORLD="${WORLD}"
+    make px4_sitl "gz_${MODEL}_${WORLD}"
+fi
